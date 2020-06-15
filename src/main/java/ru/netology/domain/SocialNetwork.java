c075516556039e751343896d8955cc3ae28853bd
Larisa Karelina <karelina.rss@gmail.com>
2020-06-15 19:28:15 +0300

    creation class v1

---
+++ src/main/java/ru/netology/domain/SocialNetwork.java
@@ -0,0 +1,17 @@
+package ru.netology.domain;
+
+public class SocialNetwork {
+
+    private String nameID;
+    private String nameLink;
+    private String DatePost;
+    private String TimePost;
+    private String image;
+    private String text;
+    private String likesCounter;
+    private String commentsInfo;
+    private String userForwardRepost;
+    private String viewsCounter;
+
+
+}
--- src/main/java/ru/netology/vk/SocialNetwork.java
+++
@@ -1,5 +0,0 @@
-package ru.netology.vk;
-
-public class SocialNetwork {
-
-}
