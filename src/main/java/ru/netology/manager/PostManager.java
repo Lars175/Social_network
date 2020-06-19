b4a5c05e93a321d32f77995e62db2feaa6861bce
Larisa Karelina <karelina.rss@gmail.com>
2020-06-19 19:35:09 +0300

    added PostManager

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,15 @@
+package ru.netology.manager;
+
+import ru.netology.domain.PostsInfo;
+
+public class PostManager {
+    private PostsInfo[] posts;
+
+    public PostsInfo[] search (int ownerId, String domain, String query, boolean ownersOnly, int count, int offset) {
+        //* TODO: add logic
+        return null;
+    }
+
+    public void delete (int ownerId, int postId) {
+    }
+}
