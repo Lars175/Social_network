fdb22734fd6f6c0f7fdefce43d3551ebb18721ad
Larisa Karelina <karelina.rss@gmail.com>
2020-06-19 19:07:57 +0300

    v2

---
+++ src/main/java/ru/netology/domain/Likes.java
@@ -0,0 +1,8 @@
+package ru.netology.domain;
+
+public class Likes {
+    private int count;
+    private int userLikes;
+    private int canLike;
+    private int canPublish;
+}
---
+++ src/main/java/ru/netology/domain/PostsInfo.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+public class PostsInfo {
+    private int Id;
+    private int ownerId;
+    private int fromId;
+    private int createdBy;
+    private int date;
+    private String text;
+    private int replyOwnerId;
+    private int replyPostId;
+    private int friendsOnly;
+    private String postType;
+    private int signerId;
+    private int canPin;
+    private int canDelete;
+    private int canEdit;
+    private int isPinned;
+    private int markedAsAds;
+    private boolean isFavorite;
+    private int postponedId;
+}
